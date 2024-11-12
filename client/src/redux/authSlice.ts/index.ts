d6c34af6b9a4deace51537ea5b9a6eadf00a6c0b
Md Emamudin <mdemamudin726@gmail.com>
2024-11-12 17:36:00 +0530

    redux config completed

--- client/src/redux/authSlice.ts/index.ts
+++ client/src/redux/authSlice.ts/index.ts
@@ -1,1 +1,40 @@
-import {createSlice} from '@reduxjs'
+import {createSlice} from '@reduxjs/toolkit'
+import { User } from '../../Types'
+
+
+interface userState {
+    user : User | null,
+    isloading : boolean,
+    isAuthenticated : boolean,
+    error : string | null
+} 
+const initialState : userState = {
+    user : null,
+    isloading : false,
+    isAuthenticated : false,
+    error : null
+}
+
+const authSlice = createSlice({
+    name : 'auth',
+    initialState,
+    reducers : {
+        signInStart:(state)=>{
+            state.isloading = true;
+        },
+        signInSuccess:(state,action)=>{
+            state.user = action.payload;
+            state.isAuthenticated = true;
+            state.isloading = false;
+            state.error = null;
+        },
+        signInFailure:(state,action)=>{
+            state.error = action.payload;
+            state.isloading = false;
+        },
+    }
+})
+
+export const {signInStart,signInFailure,signInSuccess} = authSlice.actions
+
+export default authSlice.reducer
